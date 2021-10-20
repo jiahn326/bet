0caa89e78628e052d850f81b7f15d4c4176d9a73
Jihyun Ahn <ahn990326@gmail.com>
2021-10-20 04:45:39 -0400

    conflict solved

--- src/main/java/com/bet/demo/DemoApplication.java
+++ src/main/java/com/bet/demo/DemoApplication.java
@@ -7,12 +7,10 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-<<<<<<< .merge_file_a21720
         System.out.println("메롱");
         System.out.println("jangkunsuguri");
         System.out.println("I CAN GO SLEEP NOW!!!!!!");
-=======
->>>>>>> .merge_file_a13112
+
         SpringApplication.run(DemoApplication.class, args);
         
     }
