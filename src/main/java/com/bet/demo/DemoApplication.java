bf752dabab77a402cdace5242bca0741a070169f
Jihyun Ahn <ahn990326@gmail.com>
2021-10-20 03:30:22 -0400

    빈칸

--- src/main/java/com/bet/demo/DemoApplication.java
+++ src/main/java/com/bet/demo/DemoApplication.java
@@ -7,8 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
     public static void main(String[] args) {
-        System.out.println("메롱");
-        System.out.println("메롱");
+        System.out.println();
         SpringApplication.run(DemoApplication.class, args);
     }
 
