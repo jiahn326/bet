39f1b24f9b01ebe20d7e7fbe368a68147252f191
Shoji Torikai <torikaishoji@gmail.com>
2021-10-25 11:43:13 -0400

    Setting - User class changes, POM dependency change (tomcat added for JSP support)

--- src/main/java/com/bet/demo/data/User.java
+++ src/main/java/com/bet/demo/data/User.java
@@ -1,36 +1,27 @@
 package com.bet.demo.data;
 
 public class User {
-    private String id;
+    private String email;
     private String fname;
     private String lname;
     private String username;
-    private String pword;
 
     public User() {
     }
 
-    public User(String id, String fname, String lname, String username, String pword) {
-        this.id = id;
+    public User(String email, String fname, String lname, String username) {
+        this.email = email;
         this.fname = fname;
         this.lname = lname;
         this.username = username;
-        this.pword = pword;
     }
 
-    public User(String fname, String lname, String username, String pword) {
-        this.fname = fname;
-        this.lname = lname;
-        this.username = username;
-        this.pword = pword;
+    public String getEmail() {
+        return email;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public String getFname() {
@@ -57,22 +48,13 @@ public class User {
         this.username = username;
     }
 
-    public String getPword() {
-        return pword;
-    }
-
-    public void setPword(String pword) {
-        this.pword = pword;
-    }
-
     @Override
     public String toString() {
         return "User{" +
-                "id='" + id + '\'' +
+                "email='" + email + '\'' +
                 ", fname='" + fname + '\'' +
                 ", lname='" + lname + '\'' +
                 ", username='" + username + '\'' +
-                ", pword='" + pword + '\'' +
                 '}';
     }
 }
