b54382583da9776975336e3d79b99e53a905e281
Jihyun Ahn <ahn990326@gmail.com>
2021-10-25 10:07:43 -0400

    Setting - Main Controller, login, signUp classes

--- src/main/java/com/bet/demo/controller/MainController.java
+++ src/main/java/com/bet/demo/controller/MainController.java
@@ -1,8 +1,52 @@
 package com.bet.demo.controller;
 
+import com.bet.demo.service.MainService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class MainController {
+    @Autowired
+    private MainService mainService;
 
+    @RequestMapping("/")
+    public String open(){
+        return "redirect:/login";
+    }
+
+//    @RequestMapping("/openTms")
+//    public String openTms(HttpServletRequest request){
+//        HttpSession session = request.getSession(false);
+//        if(session != null){
+//            SessionVO sesionVO = (SessionVO) session.getAttribute("SessionVO");
+//            if(sesionVO == null ){
+//                return "loginView/login";
+//            }else{
+//                return "content";
+//            }
+//        }else{
+//            return "loginView/login";
+//        }
+//    }
+//
+    @RequestMapping("/login")
+    public ModelAndView loginCheck(HttpServletRequest request, UserVO vo){
+        HttpSession session = request.getSession(false);
+        ModelAndView mv = new ModelAndView();
+        mv.setViewName("loginView/login");
+        if(session != null){
+            SessionVO sessionVO = (SessionVO) session.getAttribute("SessionVO");
+            if(sessionVO == null) {
+                mv.addObject("userId", vo.getUserId());
+                return mv;
+            }else {
+                mv.setViewName("content");
+                return mv;
+            }
+        }else{
+            mv.addObject("userId",vo.getUserId());
+            return mv;
+        }
+    }
 }
