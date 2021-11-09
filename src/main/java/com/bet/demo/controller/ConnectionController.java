25118561fd507aefb568e4fedc07c1a993781934
Jihyun Ahn <ahn990326@gmail.com>
2021-11-09 13:58:48 -0500

    budget, calendar, chart - Create pages

--- src/main/java/com/bet/demo/controller/ConnectionController.java
+++ src/main/java/com/bet/demo/controller/ConnectionController.java
@@ -6,8 +6,23 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class ConnectionController {
     @RequestMapping("/history/info")
-    public String connectionMailInfoView(){
+    public String connectionHistoryInfoView(){
         return "historyView/history";
+    }
+
+    @RequestMapping("/chart/info")
+    public String connectionChartInfoView(){
+        return "chartView/chart";
+    }
+
+    @RequestMapping("/calendar/info")
+    public String connectionCalendarInfoView(){
+        return "calendarView/calendar";
+    }
+
+    @RequestMapping("/budget/info")
+    public String connectionBudgetInfoView(){
+        return "budgetView/budget";
     }
 
 }
