6df7d8b189e332582898372f49521dc070a0b3ec
Shoji Torikai <shoji2gen@gmail.com>
2021-11-17 11:55:05 -0500

    [Backend] - Basic Sign Up Implementation
    signUp.jsp - changed to add id to form and input
    MainService.java - method added to grab data from JSP form and add data to Firebase
    MainController.java - method added to call MainService signUp method
    DemoApplication.java - TEMPORARY change, Firebase init was moved to Controller signUp method

--- src/main/java/com/bet/demo/service/MainService.java
+++ src/main/java/com/bet/demo/service/MainService.java
@@ -1,8 +1,65 @@
 package com.bet.demo.service;
 
+import com.google.api.core.ApiFuture;
+import com.google.cloud.firestore.DocumentReference;
+import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteResult;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.ExecutionException;
 
 @Service
 public class MainService {
 
+    @RequestMapping(value = "/signUp")
+    public void signUp(HttpServletRequest request, HttpServletResponse response, HttpSession session, Firestore db) throws ExecutionException, InterruptedException {
+        // HTML 폼에서 username으로 전달된 값을 가지고 옴
+        String nickname = request.getParameter("nickname");
+        String firstname = request.getParameter("firstname");
+        String lastname = request.getParameter("lastname");
+        String password = request.getParameter("password");
+        String email = request.getParameter("email");
+
+        DocumentReference docRef = db.collection("users").document("test1"); //***need to find a unique string to replace childpath***
+        // Add document data  with id "alovelace" using a hashmap
+        Map<String, Object> data = new HashMap<>();
+
+        // nickname이 null 이 아닌 경우 세션에 값을 저장
+        if(nickname != null) {
+            data.put("nickname", nickname);
+        }
+
+        // firstname이 null 이 아닌 경우 세션에 값을 저장
+        if(firstname != null) {
+            data.put("firstname", firstname);
+        }
+
+        // lastname이 null 이 아닌 경우 세션에 값을 저장
+        if(lastname != null) {
+            data.put("lastname", lastname);
+        }
+
+        // password이 null 이 아닌 경우 세션에 값을 저장
+        if(password != null) {
+            data.put("password", password);
+        }
+
+        // email이 null 이 아닌 경우 세션에 값을 저장
+        if(email != null) {
+            data.put("email", email);
+        }
+
+        //asynchronously write data
+        ApiFuture<WriteResult> result = docRef.set(data);
+        // ...
+        // result.get() blocks on response
+        System.out.println("Update time : " + result.get().getUpdateTime());
+
+    }
 }
